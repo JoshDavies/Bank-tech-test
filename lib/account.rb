69d152b392d415491b86b453b9de5439f0e24c65
Josh Davies <jdaviescreative@gmail.com>
2019-11-18 14:47:58 +0000

    balance now saved as BigDecimal to avoid float calculation errors

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+require 'money'
 
 class Account
+  def initialize
+    @balance = BigDecimal('0.00')
+  end
+
   def current_balance
-    0
+    '%.2f' % (@balance)
   end
 end
